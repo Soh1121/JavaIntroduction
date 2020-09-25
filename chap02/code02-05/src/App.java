2f1a656b45f6f0d10c379755bcdb7bda42b7011b
Soh1121 <satou.shg@gmail.com>
2020-09-25 13:30:52 +0900

    [add]値より大きな型の変数への代入の動作確認

---
+++ chap02/code02-05/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        float f = 3;
+        double d = f;
+        System.out.println(f);
+        System.out.println(d);
+    }
+}
