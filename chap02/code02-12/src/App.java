2771d66c3fb8913cbfebcb3448f2a306950eec01
Soh1121 <satou.shg@gmail.com>
2020-10-02 13:25:19 +0900

    [add]大きい方の数値を代入する命令

---
+++ chap02/code02-12/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int a = 5;
+        int b = 3;
+        int m = Math.max(a, b);
+        System.out.println("比較実験：" + a + "と" + b + "とで大きいほうは…" + m);
+    }
+}
