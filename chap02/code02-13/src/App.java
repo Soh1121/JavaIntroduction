ef7b3e8f12141673df7861bcf859b02697099d9a
Soh1121 <satou.shg@gmail.com>
2020-10-02 13:28:09 +0900

    [add]String型をint型に変換する命令

---
+++ chap02/code02-13/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String age = "31";
+        int n = Integer.parseInt(age);
+        System.out.println("あなたは来年、" + (n + 1) + "歳になりますね。");
+    }
+}
