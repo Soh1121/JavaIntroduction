539df0e12e7b3488652a42c7fe36b351ea3594a2
Soh1121 <satou.shg@gmail.com>
2020-12-18 16:53:35 +0900

    [add]同じ配列を参照していることを確認

---
+++ chap05/code05-13/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    // int型配列を受け取り、
+    // 配列内の要素すべてに1を加えるメソッド
+    public static void incArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+        array[i]++;
+        }
+    }
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3};
+        for (int i : array) {
+            System.out.println(i);
+        }
+        incArray(array);
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+}
