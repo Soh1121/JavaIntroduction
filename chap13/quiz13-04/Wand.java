75e01f4821d3864feda121605d392158c08ab2d7
Soh1121 <satou.shg@gmail.com>
2021-04-09 14:52:36 +0900

    [add]練習13-4を解答

--- chap13/quiz13-04/Wand.java
+++ chap13/quiz13-04/Wand.java
@@ -5,18 +5,12 @@ public class Wand {
     return this.name;
   }
   public void setName(String name) {
-    if(name == null || name.length() < 3) {
-      throw new IllegalArgumentException("杖に設定すべき名前は3文字以上です。");
-    }
     this.name = name;
   }
   public double getPower() {
     return this.power;
   }
   public void setPower(double power) {
-    if(power < 0.5 && 100 < power) {
-      throw new IllegalArgumentException("杖による増幅率は0.5以上100以下です。");
-    }
     this.power = power;
   }
 }
